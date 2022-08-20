bdc8e7fec7f8791e8062f482f0d2416cd4ea0d1d
R4m1q <50233975+r4m1q@users.noreply.github.com>
2022-08-20 04:12:57 +0400

    Add files via upload

---
+++ imtahansuallari.py
@@ -0,0 +1,29 @@
+name = input("Adin nedir kishi? ")
+answer1 = input("En boyuk heyvan hansidir? ")
+if answer1 == "fil":
+    print("Ogulsan!")
+else:
+    print(f"Cavab 'fil-dir', {answer1!r} deyil!")
+
+
+answer2 = input("Bir cox telebe ne ucun Python oyrenir? ")
+if answer2 == "trenddir deye":
+    print("Ogulsan!")
+else:
+    print(f"Cavab 'trenddir deye-dir', {answer2!r} deyil!")
+
+
+answer3 = input("Cinler insanlardan ne ile ferqlenirler? ")
+if answer3 == "teyyul-erz ile":
+    print("Ogulsan!")
+else:
+    print(f"Cavab 'teyyul-erz ile-dir', {answer3!r} deyil!")
+
+
+answer4 = input("Yalanin omru neche gundur? ")
+if answer4 == "40 gun":
+    print("Ogulsan!")
+else:
+    print(f"Cavab '40 gun-dur', {answer4!r} deyil!")
+
+    print("Lale xanim: Imtahan sona catdi! dincel canavar!")
